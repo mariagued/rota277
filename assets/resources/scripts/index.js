e419362a87a5ea8e30b1b0a76f077f2211008352
Maria Eduarda Guedes <151767705+mariagued@users.noreply.github.com>
2025-05-27 22:30:30 -0300

    Atualização do Projeto

---
+++ assets/resources/scripts/index.js
@@ -0,0 +1,5 @@
+  document.addEventListener('DOMContentLoaded', function() {
+    const elems = document.querySelectorAll('.sidenav');
+    const instances = M.Sidenav.init(elems, options);
+  });
+        
